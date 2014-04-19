226fa10d179a222749ca317041efcbbcc2deb69a
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2014-04-19 00:09:03 -0400

    adds reaction class, problems though

--- js/CellBiology.js
+++ js/CellBiology.js
@@ -2,20 +2,24 @@
 function Molecule(moleculeName, mtype){
 	this.molName = moleculeName;
 	this.molType = mtype;
+	this.isOrganic = true;
 	this.setType = function(mtype){
 		this.molType = mtype;
 	}
-}
-function Protein(proteinName, domains){
-	this.proteinName = proteinName;
-	var proteinType = null;
-	if(typeof(domains)===undefined){
-		var domains = [];
-	}else{
-		var domains = domains;
+	this.setOrganic = function(bool){
+		if(typeof(bool)=='boolean'){
+			this.isOrganic = bool;
+		}
 	}
+}
+function Protein(proteinName, ptype){
+	this.proteinName = proteinName;
+	var proteinType = ptype;
+	var domains = [];
 	var activators = [];
 	var inhibitors = [];
+
+	//setters
 	this.setType = function(ptype){
 		proteinType = ptype;
 	} 
@@ -25,6 +29,8 @@ function Protein(proteinName, domains){
 	this.setInhibitors = function(arg){
 		inhibitors=arg;
 	}
+
+	//adders
 	this.addActivator = function(arg){
 		activators.push(arg);
 	}
@@ -34,6 +40,8 @@ function Protein(proteinName, domains){
 	this.addDomain = function(newdomain){
 		domains.push(newdomain);
 	}
+
+	//getters
 	this.getDomains = function(){
 		return domains;
 	}
@@ -53,11 +61,57 @@ function Protein(proteinName, domains){
 			return proteinType;
 		}
 	}
+}//end Protein
 
+function Reaction(reactants, products, catalyst){
+	if(typeof(reactants)== undefined || typeof(products)==undefined){
+		this.reactants = ['error'];
+		this.products = ['error'];
+	}else{
+		if(reactants instanceof Protein){
+			this.reactants = [reactants];
+		}else if(reactants instanceof Array){
+			this.reactants=reactants;
+		}
+		if(products instanceof Protein){
+			this.products=[products];
+		}else if(products instanceof Array){
+			this.products=products;
+		}
+	}
+	if(typeof(catalyst)!=undefined && catalyst.getType=='catalyst'){
+		this.catalyst = catalyst;
+	}
 }
+
+Reaction.prototype.toString = function(){
+		var s="";
+		var k=0;
+		for(reactant in this.reactants){
+			if(k>=1){
+				s+=" + ";
+			}
+			s+=reactant;
+			k++;
+		}
+		k=0;
+		s+=" --"+this.catalyst.getName()+"--> ";
+		for(product in this.products){
+			if(k>1){
+				s+=" + ";
+			}
+			s+=product;
+			k++;
+		}
+		return s;
+	}
 //tester function for quick printing
 var p = function(x){
 	console.log(x);
 }
 
-
+var fumarate = new Molecule("Fumarate");
+var malate = new Molecule("Malate");
+var cat = new Protein("Fumarase", "catalyst");
+var myreaction = new Reaction(fumarate,malate,cat);
+p(myreaction);
