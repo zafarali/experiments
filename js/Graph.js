67eacecb636f52000391cba6135f4dff0494522c
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2014-04-21 13:48:56 -0400

    adds some initial methods

--- js/Graph.js
+++ js/Graph.js
@@ -1,28 +1,95 @@
 function Graph(){
+	var nodesList = {};
 
+	//adders
+	this.addNode = function(content, key){
+		var newNode = new Node(content,key);
+		nodesList[key]=newNode;
+	}
+
+	//getters
+	this.getNode = function(key){
+		return nodesList[key];
+	}
+
+	/*this.contains = function(targetNode){
+		for(node in nodesList){
+			if(node.getContent()==targetNode.getContent()){
+				return true;
+			}
+		}
+		return false;
+	}*/
+	this.size = function(){
+		return nodesList.length;
+	}
+
+	this.addEdge = function(from, to, weight){
+		if(typeof(weight)!='number'){
+			weight=1;
+		}
+
+		//if(!this.contains(from)){
+			//this.addNode(from);
+		//}
+		//if(!this.contains(to)){
+		//	this.addNode(to);
+		//}
+
+		nodesList[from.getKey()].addNeighbour(to,weight);
+	}
+
+	this.getNodes = function(){
+		return nodesList;
+	}
 }
 
 function Node(content,key){
 	var content = content;
-	this.key = key;
-	var neighbours = [];
+	var key = key;
+	var neighbours = {};
 	//getters
+
 	this.getContent = function(){
 		return content;
 	}
 	this.getNeighbours = function(){
-		return neighbours;
+		return this.neighbours;
 	}
 	//adders
-	this.addNeighbours = function(node,weight){
+	this.addNeighbour = function(node,weight){
 		if(typeof(weight)!='number'){
-		neighbours[node] = 1;
+			this.neighbours[node] = 1;
 		}else{
-		neighbours[node]=weight;
+			this.neighbours[node]=weight;
 		}
+	}
+	this.getKey = function(){
+		return key;
+	}
+	this.getWeight = function(between){
+		return this.neighbours[between];
 	}
 	//setters
 	this.setContent = function(newcontent){
 		content = newcontent
 	}
+}
+
+//testing
+
+var p = function(o){
+	console.log(o);
+}
+var g = new Graph();
+p(g.size());
+
+var mynode1 = new Node("hello", 2);
+var mynode2 = new Node("bye", 1);
+g.addNode(mynode1);
+g.addNode(mynode2);
+g.addEdge(mynode1,mynode2, 4);
+var no = g.getNodes();
+for(x in no){
+	p(x.getContent());
 }
