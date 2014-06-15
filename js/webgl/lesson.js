b337b837a9cda0da543cc935c24c87a21b763fac
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2014-06-15 16:47:24 -0400

    completes moving square

--- js/webgl/lesson.js
+++ js/webgl/lesson.js
@@ -5,8 +5,16 @@ Guided Tutorial from Mozilla Developer Network
 
 var gl,//stores the Canvas Context, in this case the webGL context
 	horizAspect = 480.0/640.0 //stores the aspect ratio
+var lastSquareUpdateTime= 0;
 
+var squareRotation = 0.0 //stores the rotation of the square
+var squareXOffset = 0.0;
+var squareYOffset = 0.0;
+var squareZOffset = 0.0;
 
+var xIncValue = 0.2;
+var yIncValue = -0.4;
+var zIncValue = 0.3;
 
 function start(){
 
@@ -28,7 +36,7 @@ function start(){
 	console.log("started");
 	initBuffers();
 	initShaders();
-	drawScene();
+	setInterval(drawScene,15);
 
 }
 
@@ -161,6 +169,11 @@ function drawScene(){
 	loadIdentity();
 	//moves away from the camera by 6 units
 	mvTranslate([-0.0,0.0,-6.0]);
+	
+	//allows us to save current mv matrix by rotating around x and z axis
+	mvPushMatrix();
+	mvRotate(squareRotation, [1,0,1]);
+	mvTranslate([squareXOffset, squareYOffset, squareZOffset]);
 
 	gl.bindBuffer(gl.ARRAY_BUFFER, squareVerticesBuffer);
 	
@@ -177,4 +190,24 @@ function drawScene(){
 	//reads items 0 through 4 in the squareVerticesBuffer
 	gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
 
+	//we restore the original matrix
+	mvPopMatrix();
+
+	//uses the amout of time passed since we last animated to determine roation
+	var currentTime = (new Date).getTime();
+	if(lastSquareUpdateTime){
+		var delta = currentTime - lastSquareUpdateTime;
+		squareRotation += (30*delta)/1000.0;
+		squareXOffset += xIncValue * ((30 * delta) / 1000.0);
+    	squareYOffset += yIncValue * ((30 * delta) / 1000.0);
+    	squareZOffset += zIncValue * ((30 * delta) / 1000.0);
+    
+    	if (Math.abs(squareYOffset) > 2.5) {
+      		xIncValue = -xIncValue;
+      		yIncValue = -yIncValue;
+      		zIncValue = -zIncValue;
+    	}
+	}
+
+	lastSquareUpdateTime = currentTime;
 }
