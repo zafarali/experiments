8ed50fcf965b2a47496eed7995776a6fcfafe78e
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2014-06-15 01:50:04 -0400

    modifies initBuffers to reflect lesson2

--- js/webgl/lesson.js
+++ js/webgl/lesson.js
@@ -128,6 +128,17 @@ function initBuffers(){
 
 	//establishes the vertices for the object
 	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+
+	var colors = [
+			1.0, 1.0, 1.0, 1.0 //white
+			1.0, 0.0, 0.0, 1.0 //red
+			0.0, 1.0, 0.0, 1.0 //green
+			0.0, 0.0, 1.0, 1.0 //blue
+			];
+
+	squareVerticesColorBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, squareVerticesColorBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
 }
 
 
